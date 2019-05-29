34c0f6381077d2320bf080bf8d5dc0b7bda89f84
Gregor Herburger <gregor.herburger@tq-group.com>
2019-05-29 14:08:41 +0200

    dirty: nor boot
    Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

--- board/tqc/tqmls1028a/tqmls1028a.c
+++ board/tqc/tqmls1028a/tqmls1028a.c
@@ -89,6 +89,11 @@ int board_early_init_f(void)
 	return 0;
 }
 
+void board_boot_order(u32 *spl_boot_list)
+{
+	spl_boot_list[0] = 6;
+}
+
 #ifdef CONFIG_OF_BOARD_SETUP
 #ifdef CONFIG_FSL_ENETC
 extern void enetc_setup(void *blob);
